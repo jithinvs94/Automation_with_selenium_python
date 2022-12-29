f96ebf8495316f9e901bbf0bc45139ee06cc3de9
jithinvs94 <31848300+jithinvs94@users.noreply.github.com>
2022-12-30 00:24:41 +0530

    latest

--- amazon.py
+++ amazon.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+from datetime import datetime, date
 import random
-# # randrange()
 
-# list1 = [android smartphone,smartphone]
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 print("--------------------------------")
 print("WebDriver Manager successfully initialized.")
@@ -16,50 +16,96 @@ driver.implicitly_wait(10)
 sleep(random.randrange(2,5))
 search_field = driver.find_element(By.ID, "twotabsearchtextbox")
 sleep(random.randrange(2,5))
-search_field.send_keys(random.choice(["android smartphone","smartphone","smartphones","android smartphones"]))
+# search_field.send_keys(random.choice(["android smartphone","smartphone","smartphones","android smartphones"]))
+search_field.send_keys("android smartphone")
 sleep(random.randrange(2,5))
 driver.find_element(By.ID, "nav-search-submit-button").click()
 sleep(random.randrange(2,5))
-# print(driver.page_source)
 # driver.find_element(By.XPATH, "//li[@id='p_n_operating_system_browse-bin/1485077031']//a[contains(.,'Android')]").click()
 # sleep(random.randrange(2,5))
 # driver.find_element(By.PARTIAL_LINK_TEXT, "Top Brand").click()
 # sleep(random.randrange(2,5))
 
-# driver.find_element(By.ID, "p_72/1318476031").click()
-# sleep(random.randrange(2,5))
-# driver.find_element(By.ID, "low-price").send_keys(random.choice([15000,17000,18000]))
-# sleep(random.randrange(2,5))
-# driver.find_element(By.ID, "high-price").send_keys(random.choice([20000,22000,24000]))
-# sleep(random.randrange(2,5))
-# driver.find_element(By.XPATH, "//li[@id='p_36/price-range']//span[@class='a-button-inner']").click()
-# # driver.find_element(By.ID, "a-autoid-1").click()
-# sleep(random.randrange(2,5))
+driver.find_element(By.ID, "p_72/1318476031").click()
+sleep(random.randrange(2,5))
+driver.find_element(By.ID, "low-price").send_keys(10000)
+sleep(random.randrange(2,5))
+driver.find_element(By.ID, "high-price").send_keys(15000)
+sleep(random.randrange(2,5))
+driver.find_element(By.XPATH, "//li[@id='p_36/price-range']//span[@class='a-button-inner']").click()
+sleep(random.randrange(2,5))
 
-# print(driver.find_element(By.XPATH, "//div[@data-component-type='s-search-result']//span[@class='a-size-base puis-light-weight-text s-link-centralized-style']").text)
-driver.find_element(By.XPATH, "//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/div/div/div/div[2]/div/div/div[1]/h2/a").click()
-# products = driver.find_elements(By.XPATH, "//div[@data-component-type='s-search-result']")
-# print(len(products))
-# for prod in products:
-#     print(prod.get_attribute('data-cel-widget'))
-#     temp = prod.find_element(By.XPATH, ".//span[@class='a-size-base puis-light-weight-text s-link-centralized-style']")
-    # if int(temp.text.strip().replace(',' , '')) > 1000:
-    #     print('fuck')
-    #     prod.click()
-    #     break
-    # elements = prod.find_elements(By.TAG_NAME, "a")
-    # for element in elements:
-    #     print(element.get_attribute('innerHTML'))
-    # # print(element.text)
+# driver.find_element(By.XPATH, '//a[contains(@class,"s-pagination-next")]').click()
+# print('hiiiiiii')
+# sleep(random.randrange(4,5))
+# driver.find_element(By.XPATH, '//a[contains(@class,"s-pagination-next")]').click()
+# sleep(random.randrange(4,5))
+# print('hiiiiiii')
+results = []
+# print(driver.find_element(By.XPATH, '//div[contains(@cel_widget_id,"MAIN-SEARCH_RESULTS-1")]').get_attribute('innerHTML'))
+ct = 0
+while ct < 20:
+    result_count = len(driver.find_elements(By.XPATH, '//div[contains(@cel_widget_id,"MAIN-SEARCH_RESULTS-")]'))
+    parent_page = driver.current_window_handle
+    for n in range(1, result_count+1):
+        try:
+            id = "MAIN-SEARCH_RESULTS-"+str(n)
+            review_count = driver.find_element(By.XPATH, '//div[contains(@cel_widget_id,\"'+id+'\")]//div[contains(@class,"s-list-col-right")]//div[@class="a-section a-spacing-none a-spacing-top-micro"]//div[@class="a-row a-size-small"]//span//span[@class="a-size-base s-underline-text"]')
+
+            if int(review_count.text.replace(',','').replace('(','').replace(')',''))>100:
+                driver.find_element(By.XPATH, '//div[contains(@cel_widget_id,\"'+id+'\")]//div[contains(@class,"s-list-col-right")]//h2//a').click()
+                driver.switch_to.window(driver.window_handles[1])
+                print(driver.title)
+                published_date = driver.find_element(By.XPATH, '//table[@id="productDetails_detailBullets_sections1"]//tr[contains(.,"Date First Available")]//td').text
+                d0 = (datetime.strptime(published_date.strip(), "%d %B %Y"))
+                d1 = datetime.today()
+                days_data = str(d1-d0).split()
+                days_old = int(days_data[0])
+                rating = (driver.find_element(By.XPATH, '//table[@id="productDetails_detailBullets_sections1"]//tr[contains(.,"Customer Reviews")]//td').text).split()
+                if 'Renewed' not in driver.find_element(By.ID, 'productTitle').text and 'Add to Cart' in driver.find_element(By.ID, 'submit.add-to-cart-announce').text and days_old<365:
+                    results.append([driver.find_element(By.XPATH, '//span[@id="productTitle"]').text, int(driver.find_element(By.XPATH, '//span[contains(@class,"priceToPay")]//span[@class="a-price-whole"]').text.replace(',','')), float(rating[2]), int(rating[0]), driver.current_url])
+                    ct += 1
+                    if ct == 20:
+                        break
+                    print(results)
+                    driver.close()
+                    driver.switch_to.window(parent_page)
+                    sleep(random.randrange(6,8))
+
+        except:
+            pass
+    print(len(results))
+    for num, result in enumerate(results):
+        print(".....")
+        print(num+1)
+        print(".....")
+        print("Name: "+result[0])
+        print("Price: "+str(result[1]))
+        print("Rating: "+str(result[2]))
+        print("Review Count: "+str(result[3]))
+        print("URL: "+result[4])
     
+    if ct < 20:
+        try:
+            driver.find_element(By.XPATH, '//a[contains(@class,"s-pagination-next")]').click()
+        except:
+            break
 
-    # print(prod.find_element(By.XPATH, "/div/div/div/div/div/div[2]/div/div/div[2]/div/span[2]/a/span"))
-#//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div[2]/div/div/div/div/div/div[2]/div/div/div[2]/div/span[2]/a/span
-# sleep(10)
-# filtered_prod1 = [prod.find_elements(By.XPATH, "/a/span").text for prod in products]
-# # print(len(filtered_prod1))
+print(len(results))
+for num, result in enumerate(results):
+    print(".....")
+    print(num+1)
+    print(".....")
+    print("Name: "+result[0])
+    print("Price: "+str(result[1]))
+    print("Rating: "+str(result[2]))
+    print("Review Count: "+str(result[3]))
+    print("URL: "+result[4])
 
-# products = driver.find_elements(By.XPATH, "//div[@data-component-type='s-search-result']/@")
+sleep(10)
+
+
+
 
 
 
